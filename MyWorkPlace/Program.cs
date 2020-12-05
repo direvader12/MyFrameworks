70441049493490f8a87696eaa6cd4782ec2093eb
Astley Dave Racho De Mesa <58480803+direvader12@users.noreply.github.com>
2020-12-05 16:45:27 +0800

    Array Activity Update

--- MyWorkPlace/Program.cs
+++ MyWorkPlace/Program.cs
@@ -10,47 +10,23 @@ using MyMiscsTools;
 namespace MyWorkPlace
 {
     //git hub 58480803+direvader12@users.noreply.github.com
+    //i am DireVader sir baka akala nyo po ibang tao hahaha sarili ko pong guthub yan
     class Program
     {
-        class MePerson
-        {
-            public string name = "Astley Dave De Mesa";
-            public int age = 17;
-            public int Getage() { return age; }
-            public string WhoIam() { return $"My Name Is {name}. "; }
-            public void HorrayPyramid(int n)
-            {
-                for (int i = 1; i <= n; i++)
-                {
-                    for (int j = i; j <= n; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int k = 1; k <= 2 * i - 1; k++)
-                    {
-                        Console.Write("*" + " ");
-                    }
-                    Console.WriteLine();
-                }
-            }
-        }
+
 
 
         static void Main(string[] args)
         {
-            MePerson Davey = new MePerson();
-            string MyName = Davey.WhoIam();
-            int Myage = Davey.Getage();
-
-            Console.WriteLine(MyName);
-            Console.WriteLine(Myage);
-            Console.WriteLine("Now you know me. How old are you?");
-            int intvalue = Convert.ToInt32(Console.ReadLine());
-            Davey.HorrayPyramid(intvalue);
-            Console.WriteLine("Great! You deserved this Pyramid");
-            Console.ReadKey();
-
+            int[] myarray = new int[3];
+            for (int i = 0; i < myarray.Length; i++)
+            {
+                myarray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The inputs are:");
+            Console.WriteLine("{0}\n{1}\n{2}", myarray[0], myarray[1], myarray[2]);
         }
     }
 }
+
 
