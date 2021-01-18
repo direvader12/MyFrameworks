fc200ab8c2a2e48bba9fcc459f259a20eed2d60a
Astley Dave Racho De Mesa <58480803+direvader12@users.noreply.github.com>
2021-01-18 12:06:48 +0800

    Last Performance Task Update

--- MyWorkPlace/Program.cs
+++ MyWorkPlace/Program.cs
@@ -30,12 +30,32 @@ namespace MyWorkPlace
         static void Main(string[] args)
         {
             Person universal = new Person();
+            universal.lastname = "error: invalid input";
+            universal.firstname = "error: invalid input";
+            universal.middlename = "error: invalid input";
+            universal.age = 0000;
+            universal.address = "error: invalid input";
+            universal.cashbalance = 0000000.00;
+            try
+            {
 
-            //universal.
-
+                universal.lastname = Console.ReadLine();
+                universal.firstname = Console.ReadLine();
+                universal.middlename = Console.ReadLine();
+                universal.age = Convert.ToInt32(Console.ReadLine());
+                universal.address = Console.ReadLine();
+                universal.cashbalance = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(universal.lastname);
+                Console.WriteLine(universal.firstname);
+                Console.WriteLine(universal.middlename);
+                Console.WriteLine(universal.age);
+                Console.WriteLine(universal.address);
+                Console.WriteLine(universal.cashbalance);
+            }
         }
-
-
     }
 }
 
